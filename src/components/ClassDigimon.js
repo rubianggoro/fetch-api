166bc57f79da47c819becfb4df2b03590ba9918c
rubianggoro <rubi.anggoro199@gmail.com>
2020-04-18 20:12:02 +0700

    Adding Using Async Await

--- src/components/ClassDigimon.js
+++ src/components/ClassDigimon.js
@@ -8,22 +8,37 @@ class ClassDigimon extends Component {
       digimon: [],
     };
   }
-  componentDidMount() {
-    axios.get("https://digimon-api.herokuapp.com/api/digimon").then((res) => {
-      const digi = res.data;
-      this.setState({ digimon: digi });
-    });
+  async componentDidMount() {
+    // axios.get("https://digimon-api.herokuapp.com/api/digimon").then((res) => {
+    //   this.setState({ digimon: res.data });
+    // });
+
+    // fetch("https://digimon-api.herokuapp.com/api/digimon")
+    //   .then((res) => res.json())
+    //   .then((data) => this.setState({ digimon: data }));
+
+    const getApi = await axios.get(
+      "https://digimon-api.herokuapp.com/api/digimon"
+    );
+    const data = await getApi.data;
+    this.setState({ digimon: data });
+
+    // const getApi = await fetch("https://digimon-api.herokuapp.com/api/digimon");
+    // const data = await getApi.json();
+    // this.setState({ digimon: data });
   }
 
   render() {
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <div>
         <h1>Ini Digimon</h1>
         <ul>
-          {this.state.digimon.map((item, index) => (
-            <li key={index}>{item.name}</li>
-          ))}
+          {this.state.digimon.map((item, index) => {
+            if (index < 10) {
+              return <li key={index}>{item.name}</li>;
+            }
+          })}
         </ul>
       </div>
     );
--- src/components/FuncDigimon.js
+++ src/components/FuncDigimon.js
@@ -1,26 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const FuncDigimon = () => {
   const [digimon, setDigimon] = useState([]);
 
-  React.useEffect(() => {
-    axios
-      .get("https://digimon-api.herokuapp.com/api/digimon")
-      .then((res) => {
-        setDigimon(res.data);
-      })
-      .catch((eror) => {
-        console.log(eror);
-      });
+  useEffect(() => {
+    // axios
+    //   .get("https://digimon-api.herokuapp.com/api/digimon")
+    //   .then((res) => {
+    //     setDigimon(res.data);
+    //   })
+    //   .catch((eror) => {
+    //     console.log(eror);
+    //   });
+
+    // fetch("https://digimon-api.herokuapp.com/api/digimon")
+    //   .then((res) => res.json())
+    //   .then((data) => setDigimon(data));
+
+    const getApi = async () => {
+      const res = await axios.get(
+        "https://digimon-api.herokuapp.com/api/digimon"
+      );
+      setDigimon(res.data);
+    };
+    getApi();
+
+    // const getApi = async () => {
+    //   const res = await fetch("https://digimon-api.herokuapp.com/api/digimon");
+    //   const json = await res.json();
+    //   setDigimon(json);
+    // };
+    // getApi();
   }, []);
 
   return (
     <div>
       <h1>Gambar Digimon</h1>
-      {digimon.map((item, index) => (
-        <img key={index} src={item.img} />
-      ))}
+      {digimon.map((item, index) => {
+        if (index < 10) {
+          return <img key={index} src={item.img} alt="img" />;
+        }
+      })}
     </div>
   );
 };
